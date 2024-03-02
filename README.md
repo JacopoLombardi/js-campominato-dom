c57ea00619702525ab2d928eae5482e84a69fa2b
JacopoLombardi <lombajacopo@gmail.com>
2024-03-02 03:41:39 +0100

    [add] Tring to do counter without bug

--- README.md
+++ README.md
@@ -10,4 +10,5 @@ Campo Minato
 5. Creare le 16 bombe univoche in base al numero di caselle.
 6. Quando viene cliccato un 'element' mostarare la casella oppure la bomba.
 7. Se viene cliccata la bomba, visualizza tutte le bombe.
-8. 
+8. Visualizzare su Dom il punteggio corrente e se si ha vinto o perso.
+9. 
--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 
+// variabili Js collegate all'HTML
 const containerCell = document.querySelector('._container_cell');
 const btnStart = document.querySelector('.btn_start');
 const btnClicked = document.querySelector('._clicked');
 const difficultSelect = document.querySelector('.difficult_select');
+// output string
+const outputFinalScore = document.querySelector('._output_final_score');
+const outputScore = document.querySelector('._output_score');
 
 
+// array
 const nCellArray = [100, 81, 49];
-
 const bombsArray = [];
-
 const elementBombArray = [];
-
+const elements = [];
 
 // al click di btnStart....
 btnStart.addEventListener('click', function(){
@@ -18,23 +21,21 @@ btnStart.addEventListener('click', function(){
    // invoco la funzione reset
    reset();
 
- 
+   let score = 0;
 
+    console.log('inizio',score)
    // con il valore della difficoltà da 0 a 2 valorizzo un numero dentro un Array
    const nCell = nCellArray[difficultSelect.value];
 
-
    // invoco la funzione per creare le bombe
    const bombs = createBombs(nCell);
-   
 
-   console.log('bombe totali in gioco -----------',bombs)
 
 
    // creazione delle Cell
    for( let i = 1; i <= nCell; i++){
       // invoco la funzione per creare le Cell
-      const cell = createCell(i, nCell);
+      const cell = createCell(i, nCell, score);
    
       containerCell.append(cell);   
    }
@@ -44,48 +45,119 @@ btnStart.addEventListener('click', function(){
 
 
 
-/////////// FUNCTIONS ///////////////
+
+
+///////////////////////// FUNCTIONS //////////////////////////
+
 
 // funzione che crea le Cell
-function createCell(indice, nCell){
+function createCell(indice, nCell, punteggio){
 
    // creo sull'HTML un 'div' e gli metto la Class '_cell'
    const element = document.createElement('div');
    element.classList.add('_cell');
 
+   // inserisco tutti gli element dentro un Array
+   elements.push(element);
 
    // invoco la funzione per sapere quale classe dare ad 'element'
    element.classList.add(cssClassEasyMediumHard(nCell));
 
-
    // creazione di una proprietà custom dell'HTML
    element._numIndice = indice;
-
+   
+   // se nell'array delle bombe è incluso il numero corrente...
    if(bombsArray.includes(element._numIndice)){
-      element._numIndice = '';
-      element._numBomb = 'bomba';
-      element.classList.add('_bomb');
-      elementBombArray.push(element);
+
+      element._numIndice = '';        // cancella il numero di '_numIndice'
+      element._numBomb = 'bomba';        // crea una proprietà custom e scrivici 'bombA'
+      elementBombArray.push(element);       // metti elemento bomba HTML dentro l''elementBombaArray'
    }
+
 
 
    // al click di 'element'....
    element.addEventListener('click', function(){
 
+      // se la proprietà custom '_numBomb' ha scritto dentro di se 'bomba'...
       if(element._numBomb === 'bomba'){
+
+         // aggiungi a tutti gli elementi bomba dell'Array la classe 'bg-danger'
          for(let i = 0; i < elementBombArray.length; i++){
             elementBombArray[i].classList.add('bg-danger');
          }
+         outputFinalScore.innerHTML = `Hai Perso!`;
       }
 
       // scrivi dentro 'element' il numero dell'indice e aggiungi ad element la Class '_bgcell'
       element.innerHTML = element._numIndice;
       element.classList.add('_bgcell');
+
+
+      isClicked(element, nCell, punteggio, elements);
+
+      
+      
    })
 
    return element;
 }
       
+
+
+
+
+
+function isClicked(element, nCell, punteggio, elements){
+
+   element.classList.add('_clicked');
+
+   
+
+   elements.splice();
+
+   for(let i = 0; i < elements.length; i++){
+
+      if(elements[i].classList.contains('_clicked')){
+         
+         punteggio++;
+         console.log(punteggio)
+      }
+   }
+
+
+
+
+   //  if(!element.classList.contains('_clicked')){
+
+   //     element.classList.add('_clicked');
+   //     console.log(element)
+
+   //     punteggio++;
+
+   //     console.log(punteggio)
+   //    }
+
+
+    if(punteggio === (nCell - 16)){
+       outputFinalScore.innerHTML = `Hai Vinto!`;
+    }
+
+    outputScore.innerHTML = `Hai fatto ${punteggio} punti su ${nCell}`;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -103,17 +175,6 @@ function createBombs(celle){
    }
    return bombsArray; 
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -138,11 +199,11 @@ function cssClassEasyMediumHard(confronto){
 
 
 
-
-
 // funzione di reset
 function reset(){
    containerCell.innerHTML = '';
+   outputScore.innerHTML = '';
+   outputFinalScore.innerHTML = '';
 }
  
    
